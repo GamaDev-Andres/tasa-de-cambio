4049a3e99f0b619c2e9aeae8fa677e566658ab1d
Andres Gama <gama_pipe@hotmail.com>
2022-07-05 16:48:16 -0500

    [backend] add new dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import cors from 'cors'
+
+import latestRouter from './routes/latest.js'
+
 const app = express()
+app.use(cors())
+app.use(express.json({ extended: true }))
+
+app.use('/api/latest', latestRouter)
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
