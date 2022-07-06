01b8a16b7ed002a5674e147d989c2bed2b871707
Andres Gama <gama_pipe@hotmail.com>
2022-07-05 20:24:10 -0500

    [frontend] add services

---
+++ frontend/src/services/endpoints.js
@@ -0,0 +1,14 @@
+const url = import.meta.env.VITE_URL_BACKEND;
+
+const endPoints = {
+  getCurrencies() {
+    return `${url}/api/currencies`;
+  },
+  getLatest(symbol) {
+    return url + `/api/latest/${symbol}`;
+  },
+  getHistorical(date, symbol) {
+    return `${url}/api/historical?date=${date}&symbol=${symbol}`;
+  },
+};
+export default endPoints;
