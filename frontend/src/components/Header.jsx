1d8c6f104d149a4792207386c0b757bb976b2768
Andres Gama <gama_pipe@hotmail.com>
2022-07-05 20:24:46 -0500

    [frontend] add Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="w-full h-12 flex items-center justify-center bg-slate-300">
+      <a className="text-center text-blue-900" href="/">
+        <h1 className="font-semibold text-xl">Tasa de cambio</h1>
+      </a>
+    </header>
+  );
+};
+
+export default Header;
